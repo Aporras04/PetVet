891cb95a596002150df237e6735f676491626482
DavidLPE <david.lucero@paymentevolution.com>
2023-12-17 17:11:40 -0500

    creacion y uso de base de datos

---
+++ Models/PetVetModels.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Xml.Linq;
+
+namespace PetVet.Models
+{    
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(60)]
+        public string Direccion { get; set; }
+
+        [Column(TypeName = "INT")]
+        [MaxLength(10)]
+        public int Cedula { get; set; }
+
+        [Column(TypeName = "INT")]
+        [MaxLength(10)]
+        public int Telefono { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Mail { get; set; }
+
+        public ICollection<Mascota> Mascotas { get; set; }   
+    }
+
+    public class Mascota
+    {
+        [Key]
+        public int MascotaID { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Raza { get; set; }
+
+        [Column(TypeName = "INT")]
+        [MaxLength(2)]
+        public int Edad { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(1)]
+        public string Sexo { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(10)]
+        public string Color { get; set; }
+
+        public bool Esterilizado { get; set; }
+        public ICollection<Veterinaria> Veterinarias { get; set; }
+        public ICollection<Tratamiento> Tratamientos { get; set; }       
+        public Usuario Usuario { get; set; }
+    }
+
+    public class Tratamiento
+    {
+        [Key]
+        public int idTratamiento { get; set; }  
+        
+        public DateTime Fecha { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Descripcion { get; set; }
+
+        [Column(TypeName = "FLOAT")]        
+        public float PesoMascota { get; set; }
+        
+        public DateTime FechaProxima { get; set; }
+
+        public Mascota Mascota { get; set; }
+
+    }
+
+    public class Veterinaria
+    {
+        [Key]
+        public int VeterinariaID { get; set; }        
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Direccion { get; set; }
+
+        [Column(TypeName = "INT")]
+        [MaxLength(10)]
+        public int Telefono { get; set; } 
+       
+        public Mascota Mascota { get; set; }
+       
+        public Doctor Doctor { get; set; }
+    }
+
+    public class Doctor
+    {
+        [Key]
+        public int DoctorID { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Nombre { get; set; }        
+
+        [Column(TypeName = "INT")]
+        [MaxLength(10)]
+        public int Cedula { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(45)]
+        public string Mail { get; set; }
+
+        public ICollection<Veterinaria> Veterinarias { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using PetVet.DAL;
 using PetVet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<PetVetContext>(options =>
+  options.UseSqlServer(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
@@ -19,6 +20,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
@@ -26,6 +28,15 @@ else
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<PetVetContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();

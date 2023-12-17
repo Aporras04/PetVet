575ba0cef5a75cc1fe20831f519bae92b6a9803b
Aporras04 <andrea.porras04@hotmail.com>
2023-12-16 22:56:05 -0500

    Create project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PetVet.Models;
+
+namespace PetVet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult LogIn()
+    {
+        return View();
+    }
+
+    public IActionResult MisMascotas()
+    {
+        return View();
+    }
+
+    public IActionResult NuevaMascota()
+    {
+        return View();
+    }
+
+    public IActionResult Perfil()
+    {
+        return View();
+    }
+
+    public IActionResult PerfilMascota()
+    {
+        return View();
+    }
+
+    public IActionResult Veterinarias()
+    {
+        return View();
+    }
+
+    public IActionResult Mascotas()
+    {
+        return View();
+    }
+
+    public IActionResult Usuarios()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PetVet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
